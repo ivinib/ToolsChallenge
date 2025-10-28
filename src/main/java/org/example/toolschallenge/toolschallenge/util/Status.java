056545e8329a2ecea736716ab642c84d617d7bbd
ivinib <vi_ni_b@hotmail.com>
2025-10-28 18:13:34 -0300

    Adicionando os ENUMS para os campos de Status e Tipo de Pagamento

---
+++ src/main/java/org/example/toolschallenge/toolschallenge/util/Status.java
@@ -0,0 +1,7 @@
+package org.example.toolschallenge.toolschallenge.util;
+
+public enum Status {
+    AUTORIZADO,
+    NEGADO,
+    CANCELADO
+}
---
+++ src/main/java/org/example/toolschallenge/toolschallenge/util/TipoPagamento.java
@@ -0,0 +1,7 @@
+package org.example.toolschallenge.toolschallenge.util;
+
+public enum TipoPagamento {
+    AVISTA,
+    PARCELADO_LOJA,
+    PARCELADO_EMISSOR
+}
