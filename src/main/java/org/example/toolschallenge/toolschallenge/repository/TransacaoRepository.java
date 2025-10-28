81d79f40f63222fc5ad6c709602dc512c8492fca
ivinib <vi_ni_b@hotmail.com>
2025-10-28 18:11:01 -0300

    Arrumando a Repository

--- src/main/java/org/example/toolschallenge/toolschallenge/repository/TransacaoRepository.java
+++ src/main/java/org/example/toolschallenge/toolschallenge/repository/TransacaoRepository.java
@@ -2,6 +2,8 @@ package org.example.toolschallenge.toolschallenge.repository;
 
 import org.example.toolschallenge.toolschallenge.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransacaoRepository  extends JpaRepository<Transacao, Long> {
 }
